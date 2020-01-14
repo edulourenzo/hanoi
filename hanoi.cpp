d83e3488ab517abf5a2247405c16b580c548a288
edulourenzo <edulourenzo@hotmail.com>
2020-01-14 14:00:28 -0300

    Estruturas básicas

--- hanoi.cpp
+++ hanoi.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 
+#define NUM_RODS 3
+#define NUM_DISKS 4
+
+typedef int rod_origin;
+typedef int rod_destiny;
+
 using namespace std;
 
 int main()
 {
+    vector< stack<char> > rod(NUM_RODS, stack<char>());
+    vector< pair<rod_origin, rod_destiny> > tree(pow(2, NUM_DISKS) - 1);
+
+
+
     cout << "Hello World!" << endl;
 
     return 0;
